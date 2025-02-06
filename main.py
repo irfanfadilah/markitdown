7910e538c0c4838f4122cc8736deed4cf2c18da1
Irfan Fadilah <hello@irfanfadilah.com>
2025-02-07 03:15:06 +0700

    Automatically Delete Tmp File

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI, UploadFile, Response
 from fastapi.middleware.cors import CORSMiddleware
 from markitdown import MarkItDown
 import tempfile
-import os
 
 app = FastAPI()
 
@@ -15,12 +14,10 @@ app.add_middleware(
 
 @app.post("/v1/markitdown")
 def process(file: UploadFile):
-  with tempfile.NamedTemporaryFile() as tmp:
+  with tempfile.NamedTemporaryFile(delete=True) as tmp:
     try:
       tmp.write(file.file.read())
       result = MarkItDown().convert(tmp.name)
       return Response(result.text_content, media_type="text/markdown", status_code=200)
     except Exception as error:
       return Response(str(error), media_type="text/plain", status_code=400)
-    finally:
-      os.unlink(tmp.name)
